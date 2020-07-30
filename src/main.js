8d1e302baeef666ba894e166f2e4214d262b615b
light-int <obamenzoghobilly@gmail.com>
2020-07-30 13:50:15 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import vueSmoothScroll from 'vue2-smooth-scroll'
-import loader from "vue-ui-preloader";
+import loader from 'vue-ui-preloader'
 
 Vue.use(loader);
 Vue.use(vueSmoothScroll)
