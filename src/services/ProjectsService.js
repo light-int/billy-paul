9c8000e5454df900e11d7e85c17984cbe6dc4fc3
light-int <44721942+light-int@users.noreply.github.com>
2024-02-24 13:10:51 +0100

    Update ProjectsService.js
    change token

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -4,7 +4,7 @@ const Axios = axios.create({
   baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets"
 });
 
-Axios.defaults.headers.common = {'Authorization': 'Bearer keyQ5sI1ohgJYlbf2'}
+Axios.defaults.headers.common = {'Authorization': 'Bearer patM28TaEprIOQkya.c52d977c237a1cffb30ad41003b5ee53ddcb01fc25ca3da643bcd5224306bf66'}
 
 export default{
     getProjects() {
