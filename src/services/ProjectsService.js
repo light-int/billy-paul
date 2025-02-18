040b6570c980054517572f5e7026fb364ebfd925
light-int <44721942+light-int@users.noreply.github.com>
2025-02-18 10:42:19 +0100

    Update ProjectsService.js

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-  baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets"
-});
+  baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets/"});
 
-Axios.defaults.headers.common = {'Authorization': 'Bearer patM28TaEprIOQkya.ea3fd79c07493ed69f90084adac1349beabf5dc414ada4b4653635b8fc30fd75'}
+Axios.defaults.headers.common = {'Authorization':'Bearer patM28TaEprIOQkya.ea3fd79c07493ed69f90084adac1349beabf5dc414ada4b4653635b8fc30fd75'}
 
 export default{
     getProjects() {
