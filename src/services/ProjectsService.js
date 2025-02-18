54c1d6dfbe616a2248079b7650d20cf81177fdac
light-int <44721942+light-int@users.noreply.github.com>
2025-02-18 10:38:24 +0100

    Update ProjectsService.js si;ple code

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -1,33 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const Axios = axios.create({
-  baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets",
-  headers: {
-    Authorization: "Bearer patM28TaEprIOQkya.63da68e6431f2f61621e4cff9048275821573fe434a98e4751268661ff8c69ef"
-  }
+  baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets"
 });
 
-const api = {
-  async getProjects() {
-    try {
-      const response = await Axios.get("/");
-      return response.data;
-    } catch (error) {
-      console.error("Erreur lors de la récupération des projets :", error);
-      throw error;
-    }
-  },
+Axios.defaults.headers.common = {'Authorization': 'Bearer patM28TaEprIOQkya.63da68e6431f2f61621e4cff9048275821573fe434a98e4751268661ff8c69ef'}
 
-  async getProject(slug) {
-    try {
-      const filter = encodeURIComponent(`{slug} = '${slug}'`);
-      const response = await Axios.get(`?filterByFormula=${filter}`);
-      return response.data;
-    } catch (error) {
-      console.error(`Erreur lors de la récupération du projet avec slug "${slug}" :`, error);
-      throw error;
+export default{
+    getProjects() {
+        return Axios.get()
+    },
+    getProject(slug) {
+        return Axios.get("?filterByFormula={slug}='" + slug + "'")
     }
-  }
-};
-
-export default api;
+}
